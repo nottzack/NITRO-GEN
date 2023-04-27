8d99ff0d1635ac9aec1e6459ec5c5233dfc42bfa
nOTTZACK <127354535+NTZKTUDEBELOPER@users.noreply.github.com>
2023-04-28 01:13:29 +0200

    Add files via upload

---
+++ NTZK-N1TRO-GEN-main (1)/NTZK-N1TRO-GEN-main/instalar.bat
@@ -0,0 +1,9 @@
+@echo off
+echo.
+echo [40;37m Pulsa alguna telca!!
+echo [30;m Support of discord.gg/noxius
+echo.
+                                                                                          
+
+timeout 1000 >nul
+python nitro.py
